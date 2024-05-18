58d9ad0689defca9c90d0a0146d61571de9033a3
dungpt <dungpt.22itb@vku.udn.vn>
2024-05-18 10:29:57 +0700

    first commit

---
+++ admin/src/main/java/com/example/admin/Data/DataProduct.kt
@@ -0,0 +1,13 @@
+package com.example.admin.Data
+
+data class ProductData(
+    var productId: String? ="",
+    var name: String?="",
+    var cost: String?="",
+    var imageUrl: String?="",
+)
+data class Userata(
+    var username: String? ="",
+    var password: String?="",
+)
+
